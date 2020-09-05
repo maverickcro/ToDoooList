8def71b6458fcb625cf57b47a5bbf6a997d692cb
maverickcro <mav3ricktv@gmail.com>
2020-09-05 20:26:11 +0200

    Updates

---
+++ precache-manifest.810b63ee136a71f9a107fdf463477835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb50740679dca80da49b65e0f483470",
+    "url": "/ToDoooList/index.html"
+  },
+  {
+    "revision": "b5fde9e5ec4f3a29f722",
+    "url": "/ToDoooList/static/css/main.bf054192.chunk.css"
+  },
+  {
+    "revision": "7912e253dbefb73f68dc",
+    "url": "/ToDoooList/static/js/2.3a9671f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoooList/static/js/2.3a9671f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fde9e5ec4f3a29f722",
+    "url": "/ToDoooList/static/js/main.bfe336c0.chunk.js"
+  },
+  {
+    "revision": "77b1206dab74f3f4d0d4",
+    "url": "/ToDoooList/static/js/runtime-main.6b7f7d8f.js"
+  }
+]);
